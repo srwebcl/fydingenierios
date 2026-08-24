e26b5dc0b5986324358d4bee91b13fd4dde8a4fb
srwebcl <contacto@srweb.cl>
2026-08-24 04:15:27 -0400

    fix: Redirigir antiguas URLs basura de wordpress al inicio para SEO

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,40 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/tag/:path*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/product/:path*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/portfolio/:path*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/category/:path*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blog/:path*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/author/:path*',
+        destination: '/',
+        permanent: true,
+      }
+    ];
+  }
 };
 
 export default nextConfig;
